1f5a41ea10ab7e7d45ab470abbb015293fd97212
peter_grohe <peter_grohe@web.de>
2021-05-10 22:52:17 +0200

    opening grader - no 440hz

--- Labware/Lab12_TuningFork/TuningFork.c
+++ Labware/Lab12_TuningFork/TuningFork.c
@@ -42,20 +42,48 @@ void DisableInterrupts(void); // Disable interrupts
 void EnableInterrupts(void);  // Enable interrupts
 void WaitForInterrupt(void);  // low power mode
 
+volatile unsigned long Counts=0; 
+
 // input from PA3, output from PA2, SysTick interrupts
 void Sound_Init(void){ 
 
 }
 
+void SysTick_Init(unsigned long period){ // priority 2
+  NVIC_ST_CTRL_R = 0;         // disable SysTick during setup
+  NVIC_ST_RELOAD_R = period-1;// reload value
+  NVIC_ST_CURRENT_R = 0;      // any write to current clears it
+  NVIC_SYS_PRI3_R = (NVIC_SYS_PRI3_R&0x00FFFFFF)|0x40000000;          
+  NVIC_ST_CTRL_R = 0x07; // enable SysTick with core clock and interrupts
+  // finish all initializations and then enable interrupts
+}
+
 // called at 880 Hz
 void SysTick_Handler(void){
-
+	GPIO_PORTA_DATA_R ^= 0x04;       // toggle PA2
+  Counts = Counts + 1;
 }
 
 int main(void){// activate grader and set system clock to 80 MHz
   TExaS_Init(SW_PIN_PA3, HEADPHONE_PIN_PA2,ScopeOn); 
   Sound_Init();         
   EnableInterrupts();   // enable after all initialization are done
+	
+	//insert portA pin 3 init here:
+	
+	SYSCTL_RCGC2_R |= 0x00000001; //activate PortA 
+	SYSCTL_RCGC2_R |= 0x00000020; //activate PortF 
+	GPIO_PORTA_DIR_R |= 0x04;   // make PA3 input PA2output
+	GPIO_PORTA_AFSEL_R &= ~0x04;// disable alt funct on PA2
+  GPIO_PORTA_DEN_R |= 0x04;   // enable digital I/O on PA2
+  GPIO_PORTA_PCTL_R = (GPIO_PORTF_PCTL_R&0xFFFFF0FF)+0x00000000;
+  GPIO_PORTA_AMSEL_R = 0;     // disable analog functionality on PF
+  //SysTick_Init(80000);        // initialize SysTick timer, every 1ms - changed from 16000
+  EnableInterrupts();         // enable after everything initialized
+	
+	
+	
+	
   while(1){
     // main program is free to perform other tasks
     // do not use WaitForInterrupt() here, it may cause the TExaS to crash
